6700517bed6cd3bdcf72e2bbacc225ebbef7b369
mdxshakil <mdxshakil1767@gmail.com>
2023-01-11 20:59:56 +0600

    Basic Server setup completed

--- index.js
+++ index.js
@@ -7,14 +7,6 @@ const port = process.env.PORT;
 app.use(express.json());
 app.use(cors());
 
-app.get('/content', async (req, res) => {
-res.status(200).send({message:'This is your content'})
-})
-app.get('/content/:id', async (req, res) => {
-res.status(200).send({message:'This is id'})
-})
-
-
 app.get('/', (req, res) => {
     res.send('Hello world - Server is running successfully!')
 })
