83f33351b64b2a7d5de2437e9942775ddde149de
mdxshakil <mdxshakil1767@gmail.com>
2023-01-15 22:07:50 +0600

    Crud operations route completed

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const cors = require('cors');
 const dotenv = require('dotenv').config();
 const port = process.env.PORT;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 //middleware
 app.use(express.json());
@@ -19,23 +19,47 @@ async function run() {
 
         //post a content
         app.post('/content', async (req, res) => {
-
+            const newContent = req.body;
+            const result = await contentCollection.insertOne(newContent);
+            res.status(200).send(result)
         })
         //get all the content
         app.get('/content', async (req, res) => {
-
+            const query = {};
+            const result = await contentCollection.find(query).toArray();
+            res.status(200).send(result);
         })
         //get a particular content
         app.get('/content/:id', async (req, res) => {
-
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await contentCollection.findOne(query);
+            res.send(result);
         })
         //delete a particular content
         app.delete('/content/:id', async (req, res) => {
-
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) };
+            const result = await contentCollection.deleteOne(filter);
+            res.send(result);
         })
         //update a particular content
         app.put('/content/:id', async (req, res) => {
-
+            const id = req.params.id;
+            const updatedData = req.body;
+            const filter = { _id: ObjectId(id) }
+            const options = {
+                upsert: true
+            }
+            const updatedDoc = {
+                $set: {
+                    title: updatedData.title,
+                    body: updatedData.body,
+                    image: updatedData.image
+                }
+            };
+            const result = await contentCollection.updateOne(filter, updatedDoc, options);
+            res.send(result);
         })
     } finally {
         // await client.close()
